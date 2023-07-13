8b180c44167e6cd2291fad99d8602fcbc5d1e78b
erickbxs <erickbxs@mgmail.com>
2023-07-13 09:44:28 -0300

    Feat - Requisito 01 - DONE

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_periods, target_time):
+    if not permanence_periods or target_time is None:
+        return None
+
+    count = 0
+    for entry, exit_time in permanence_periods:
+        if (
+            isinstance(entry, int)
+            and isinstance(exit_time, int)
+            and entry <= exit_time
+        ):
+            if entry <= target_time <= exit_time:
+                count += 1
+        else:
+            return None
+
+    return count
