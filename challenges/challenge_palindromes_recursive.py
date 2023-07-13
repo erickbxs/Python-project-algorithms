a707b74844190866768d803aff89e576e8fbbcc6
erickbxs <erickbxs@mgmail.com>
2023-07-13 13:49:40 -0300

    Feat: Requisito 3 - Palíndromos( Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:  # Verificação caso a palavra venha vazia
+        return False
+    if low_index >= high_index:  # Chegamos ao centro da palavra
+        return True
+    elif word[low_index] == word[high_index]:  # Chegamos recursivamente a func
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:  # Não é um palíndromo
+        return False
