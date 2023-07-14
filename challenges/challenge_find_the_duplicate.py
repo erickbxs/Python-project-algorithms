701b6cecf0f82134a32b6df22829d1cc665190a7
erickbxs <erickbxs@mgmail.com>
2023-07-14 18:49:39 -0300

    Feat: Requisito 05 - Bonus - Algo. Busca

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str):
+        return False
+
+    num_set = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        if num in num_set:
+            return num
+        num_set.add(num)
+
+    return False
